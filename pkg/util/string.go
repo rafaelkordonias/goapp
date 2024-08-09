f004f4ea7dbbec3c1564298ff63357ab1b789948
Rafael Kordonias <rafaelkordonias@gmail.com>
2024-08-09 10:45:55 +0300

    Modify random string function(RandString) to generate only hex values

--- pkg/util/string.go
+++ pkg/util/string.go
@@ -2,32 +2,33 @@ package util
 
 import (
 	"math/rand"
+	"strings"
 )
 
 var randx = rand.NewSource(42)
 
 // RandString returns a random string of length n.
 func RandString(n int) string {
-	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	const hexBytes = "0123456789abcdef"
 	const (
-		letterIdxBits = 6                    // 6 bits to represent a letter index
-		letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-		letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+		hexIdxBits = 4                 // 4 bits to represent a hex index
+		hexIdxMask = 1<<hexIdxBits - 1 // All 1-bits, as many as hexIdxBits
+		hexIdxMax  = 63 / hexIdxBits   // # of hex indices fitting in 63 bits
 	)
 
 	b := make([]byte, n)
 	// A rand.Int63() generates 63 random bits, enough for letterIdxMax letters!
-	for i, cache, remain := n-1, randx.Int63(), letterIdxMax; i >= 0; {
+	for i, cache, remain := n-1, randx.Int63(), hexIdxMax; i >= 0; {
 		if remain == 0 {
-			cache, remain = randx.Int63(), letterIdxMax
+			cache, remain = randx.Int63(), hexIdxMax
 		}
-		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
-			b[i] = letterBytes[idx]
+		if idx := int(cache & hexIdxMask); idx < len(hexBytes) {
+			b[i] = hexBytes[idx]
 			i--
 		}
-		cache >>= letterIdxBits
+		cache >>= hexIdxBits
 		remain--
 	}
-
-	return string(b)
+	result := strings.ToUpper(string(b))
+	return result
 }
